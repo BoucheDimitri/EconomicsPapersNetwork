4261d33a14a1d58cc0bc582fa472979d8cbfef55
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-05-15 11:21:00 +0200

    Added possibility of request of the type similar to (list of articles number) for subgraph construction

--- HubsAuths.py
+++ HubsAuths.py
@@ -16,6 +16,7 @@ def topic_query(attrs_df, query_string, search_in=("title", "keywords")):
     :param attrs_df: (pandas.core.frame.DataFrame) Dataframe on which to perform the query
     :param query_string: (str) The query string
     :param search_in: (tuple) The columns to include for the query
+
     :return: list : the list of result indexes, one per column in search_in
     """
     inds = []
@@ -31,6 +32,7 @@ def indexlist_inter(indexlist):
     Intersection of all elements of a list of indexes
 
     :param indexlist: (list) : the list of pandas indexes (pandas.indexes.range.RangeIndex)
+
     :return: The intersected index ((pandas.indexes.range.RangeIndex)
     """
     first = True
@@ -48,6 +50,7 @@ def indexlist_union(indexlist):
     Union of all elements of a list of indexes
 
     :param indexlist: (list) : the list of pandas indexes (pandas.indexes.range.RangeIndex)
+
     :return: The union index (pandas.indexes.range.RangeIndex)
     """
     first = True
@@ -60,14 +63,15 @@ def indexlist_union(indexlist):
     return inter
 
 
-def subgraph_root(attrs_df, query_string, search_in=("title", "keywords"), how="union"):
+def topic_subgraph_root(attrs_df, query_string, search_in=("title", "keywords"), how="union"):
     """
-    Root nodes for the subgraph generation of Hubs and Authorities
+    Root nodes for building a subgraph relevant to a topic based query
 
     :param attrs_df: (pandas.core.frame.DataFrame) Dataframe on which to perform the query
     :param query_string: (str) The query string
     :param search_in: (tuple) The columns to include for the query
     :param how: (str) How to join the indexes in the list ?
+
     :return: the index of the nodes (pandas.indexes.range.RangeIndex)
     """
     inds = topic_query(attrs_df, query_string, search_in)
@@ -77,6 +81,23 @@ def subgraph_root(attrs_df, query_string, search_in=("title", "keywords"), how="
         return indexlist_union(inds)
 
 
+def similarity_subgraph_root(nodes_list, graph):
+    """
+    Root nodes for building a subgraph relevant to similiraty query
+
+    :param nodes_list: the list of articles of our similar to request
+    :param graph: (networkx.classes.digraph.DiGraph) the graph
+
+    :return list of root nodes for our similarity request
+    """
+    root_nodes = []
+    for node in nodes_list:
+        successors = list(graph.successors(node))
+        predecessors = list(graph.predecessors(node))
+        root_nodes += successors + predecessors
+    return list(set(root_nodes))
+
+
 def expand_root(root_nodes, graph, d):
     """
     Expand root nodes by including their successors and some of their predecessors (d to be exact)
@@ -84,6 +105,7 @@ def expand_root(root_nodes, graph, d):
     :param root_nodes: (list-like) the roots nodes
     :param graph: (networkx.classes.digraph.DiGraph) the graph
     :param d: how many predecessors to include at most ?
+
     :return: the expanded nodes list (list).
     """
     nodes = []
@@ -101,7 +123,7 @@ def expand_root(root_nodes, graph, d):
     return nodes
 
 
-def query_subgraph(graph, d, attrs_df, query_string, search_in=("title", "keywords"), how="union"):
+def topic_query_subgraph(graph, d, attrs_df, query_string, search_in=("title", "keywords"), how="union"):
     """
     Find expanded subgraph for a query
 
@@ -111,9 +133,25 @@ def query_subgraph(graph, d, attrs_df, query_string, search_in=("title", "keywor
     :param query_string: (str) The query string
     :param search_in: (tuple) The columns to include for the query
     :param how: (str) How to join the indexes in the list ?
+
     :return: (networkx.classes.digraph.DiGraph) the expanded subgraph for the query
     """
-    root_nodes = subgraph_root(attrs_df, query_string, search_in, how)
+    root_nodes = topic_subgraph_root(attrs_df, query_string, search_in, how)
+    expanded = expand_root(root_nodes, graph, d)
+    return graph.subgraph(expanded)
+
+
+def similarity_query_subgraph(nodes_list, graph, d):
+    """
+    Find expanded subgraph for a query
+
+    :param graph: (networkx.classes.digraph.DiGraph) the graph
+    :param d: how many predecessors to include at most ?
+    :param nodes_list: the list of articles of our similar to request
+
+    :return: (networkx.classes.digraph.DiGraph) the expanded subgraph for the similarity query
+    """
+    root_nodes = similarity_subgraph_root(nodes_list, graph)
     expanded = expand_root(root_nodes, graph, d)
     return graph.subgraph(expanded)
 
@@ -124,6 +162,7 @@ def iterate_hubs_auths(subgraph, k=20):
 
     :param subgraph: a subgraph (networkx.classes.digraph.DiGraph)
     :param k: number of iterations
+
     :return: x, y, nodes respectively vector of authorities coefs, hubs coefs and nodes ordering used for computations
     """
     nodes = list(subgraph)
@@ -146,6 +185,7 @@ def sort_nodes(xy, nodes_list):
 
     :param xy: authorities coefs vector or hubs coefs vector
     :param nodes_list: list of actual nodes in bijection with the range index of xy
+
     :return: nodes from nodes list sorted by authorities coefs or hubs coefs depending on what xy is.
     """
     xind = np.argsort(xy)
@@ -194,18 +234,45 @@ all_edges = cits_edges + refs_edges
 # Construct nx.DiGraph from stacked edges (refs + cits)
 cits_refs_graph = nx.DiGraph(all_edges)
 
+
+# ***************TEST ON TOPIC QUERY*********************************
+
 # Create the expanded subgraph on which to perform the algo
-d = 10000
+d = 1000
 qstring = "agency problems"
-subtest = query_subgraph(cits_refs_graph, d, attrs, qstring)
+subtest_topic = topic_query_subgraph(cits_refs_graph, d, attrs, qstring)
 
 # compute hubs and authorities in an iterative fashion
-x, y, nodes = iterate_hubs_auths(subtest, k=20)
+x, y, nodes = iterate_hubs_auths(subtest_topic, k=20)
 
 # nodes sorted by authority coef
-top_auths = sort_nodes(x, nodes)
-print(top_auths)
+top_auths_topic = sort_nodes(x, nodes)
+print(top_auths_topic)
 
 # nodes sorted by "hubness" coef
-top_hubs = sort_nodes(y, nodes)
-print(top_hubs)
+top_hubs_topic = sort_nodes(y, nodes)
+print(top_hubs_topic)
+
+
+# ***************TEST ON SIMILARITY QUERY*******************************
+
+# Create the expanded subgraph on which to perform the algo
+d = 1000
+pages = [3168]
+subtest_similarity = similarity_query_subgraph(pages, cits_refs_graph, d)
+
+# compute hubs and authorities in an iterative fashion
+x, y, nodes = iterate_hubs_auths(subtest_similarity, k=20)
+
+# nodes sorted by authority coef
+top_auths_similarity = sort_nodes(x, nodes)
+print(top_auths_similarity)
+
+# nodes sorted by "hubness" coef
+top_hubs_similarity = sort_nodes(y, nodes)
+print(top_hubs_similarity)
+
+
+
+
+
